c8722b30185fa46a9762cd3036d03080155b718d
Mark George <95744991+Mark1George@users.noreply.github.com>
2025-01-12 11:18:24 +0200

    1
    1

---
+++ contact.php
@@ -0,0 +1,232 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>FALCON  CONTRACTING CO. LLC</title>
+<meta name="generator" content="FALCON - CONSTRUCTION LLC">
+<style type="text/css">
+body
+{
+   background-color: #FFFFFF;
+   color: #000000;
+}
+</style>
+<style type="text/css">
+a
+{
+   color: #388E8E;
+   text-decoration: underline;
+}
+a:visited
+{
+   color: #D2B48C;
+}
+a:active
+{
+   color: #388E8E;
+}
+a:hover
+{
+   color: #388E8E;
+   text-decoration: underline;
+}
+</style>
+<style type="text/css">
+#wb_Form1
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+}
+#wb_Text1 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+}
+#wb_Text1 div
+{
+   text-align: left;
+}
+#Editbox1
+{
+   border: 1px #C0C0C0 dotted;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#wb_Text2 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+}
+#wb_Text2 div
+{
+   text-align: left;
+}
+#Editbox2
+{
+   border: 1px #C0C0C0 dotted;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#wb_Text3 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+}
+#wb_Text3 div
+{
+   text-align: left;
+}
+#Editbox3
+{
+   border: 1px #C0C0C0 dotted;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#wb_Text4 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+}
+#wb_Text4 div
+{
+   text-align: left;
+}
+#Editbox4
+{
+   border: 1px #C0C0C0 dotted;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#wb_Text5 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+}
+#wb_Text5 div
+{
+   text-align: left;
+}
+#Editbox5
+{
+   border: 1px #C0C0C0 dotted;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+   vertical-align: middle;
+}
+#wb_Text6 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+}
+#wb_Text6 div
+{
+   text-align: left;
+}
+#TextArea1
+{
+   border: 1px #C0C0C0 dotted;
+   background-color: #FFFFFF;
+   color :#000000;
+   font-family: Arial;
+   font-size: 13px;
+   text-align: left;
+}
+#Button1
+{
+   color: #000000;
+   font-family: Arial;
+   font-size: 13px;
+}
+</style>
+<script type="text/javascript">
+<!--
+function Validatecontact(theForm)
+{
+   var regexp;
+   if (theForm.Editbox1.value == "")
+   {
+      alert("Please complete: - Name - Email - Tel");
+      theForm.Editbox1.focus();
+      return false;
+   }
+   regexp = /^[-+]?\d*\.?\d*$/;
+   if (!regexp.test(theForm.Editbox4.value))
+   {
+      alert("Please complete: - Name - Email - Tel");
+      theForm.Editbox4.focus();
+      return false;
+   }
+   if (theForm.Editbox4.value == "")
+   {
+      alert("Please complete: - Name - Email - Tel");
+      theForm.Editbox4.focus();
+      return false;
+   }
+   regexp = /^([0-9a-z]([-.\w]*[0-9a-z])*@(([0-9a-z])+([-\w]*[0-9a-z])*\.)+[a-z]{2,6})$/i;
+   if (!regexp.test(theForm.Editbox5.value))
+   {
+      alert("Please complete: - Name - Email - Tel");
+      theForm.Editbox5.focus();
+      return false;
+   }
+   if (theForm.Editbox5.value == "")
+   {
+      alert("Please complete: - Name - Email - Tel");
+      theForm.Editbox5.focus();
+      return false;
+   }
+   return true;
+}
+//-->
+</script>
+</head>
+<body>
+<div id="wb_Form1" style="position:absolute;left:3px;top:5px;width:409px;height:315px;z-index:13;">
+<form name="contact" method="post" action="contact.php" enctype="multipart/form-data" id="Form1" onsubmit="return Validatecontact(this)">
+<div id="wb_Text1" style="position:absolute;left:10px;top:15px;width:100px;height:19px;z-index:0;">
+<span style="color:#000000;font-family:'Open Sans';font-size:16px;">Name:</span></div>
+<input type="text" id="Editbox1" style="position:absolute;left:180px;top:21px;width:198px;height:23px;line-height:23px;z-index:1;" name="Name" value="">
+<div id="wb_Text2" style="position:absolute;left:10px;top:45px;width:131px;height:19px;z-index:2;">
+<span style="color:#000000;font-family:'Open Sans';font-size:16px;">Company name:</span></div>
+<input type="text" id="Editbox2" style="position:absolute;left:180px;top:51px;width:198px;height:23px;line-height:23px;z-index:3;" name="Company name" value="">
+<div id="wb_Text3" style="position:absolute;left:10px;top:75px;width:100px;height:19px;z-index:4;">
+<span style="color:#000000;font-family:'Open Sans';font-size:16px;">City:</span></div>
+<input type="text" id="Editbox3" style="position:absolute;left:180px;top:81px;width:198px;height:23px;line-height:23px;z-index:5;" name="City" value="">
+<div id="wb_Text4" style="position:absolute;left:10px;top:105px;width:100px;height:19px;z-index:6;">
+<span style="color:#000000;font-family:'Open Sans';font-size:16px;">Tel:</span></div>
+<input type="text" id="Editbox4" style="position:absolute;left:180px;top:111px;width:198px;height:23px;line-height:23px;z-index:7;" name="Tel" value="">
+<div id="wb_Text5" style="position:absolute;left:10px;top:135px;width:100px;height:19px;z-index:8;">
+<span style="color:#000000;font-family:'Open Sans';font-size:16px;">Email:</span></div>
+<input type="text" id="Editbox5" style="position:absolute;left:180px;top:141px;width:198px;height:23px;line-height:23px;z-index:9;" name="email" value="">
+<div id="wb_Text6" style="position:absolute;left:10px;top:165px;width:100px;height:19px;z-index:10;">
+<span style="color:#000000;font-family:'Open Sans';font-size:16px;">Message:</span></div>
+<textarea name="Message" id="TextArea1" style="position:absolute;left:180px;top:171px;width:198px;height:98px;z-index:11;" rows="5" cols="27"></textarea>
+<input type="submit" id="Button1" name="" value="Submit" style="position:absolute;left:180px;top:276px;width:96px;height:25px;z-index:12;">
+</form>
+</div>
+</body>
+</html>
